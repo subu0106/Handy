6fb1941062903a4933bb07e6ad8c5422ecab53d4
Pranavan-S <subendiranp@gmail.com>
2025-05-13 00:41:31 +0530

    Implement getOfferById

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createRequest } = require('../controllers/requestController');
+const { createRequest, getRequestById } = require('../controllers/requestController');
 const router = express.Router();
 
 router.route("/createRequest").post(createRequest);
+router.route("/:request_id").get(getRequestById);
 
 module.exports = {router};
