bac2cc99efc6d4b8feb39ef0f26c450c038e01d5
Pranavan-S <subendiranp@gmail.com>
2025-05-12 22:01:05 +0530

    Remove unused functions

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/v1", router);
-app.use(require("./src/middleware/sampleErrorHandler"));
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
