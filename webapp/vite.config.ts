7df45fd3b6c7fde3005681a66c638dc683cc03ed
patricnilackshan <nilackshanp@gmail.com>
2025-05-16 01:22:37 +0530

    build: configure TypeScript path aliases with Vite

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+      '@routes': '/src/routes',
+      '@utils': '/src/utils',
+      '@layouts': '/src/layouts',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+    }
+  }
 })
