b17c899eeaf19fdd8559fd9907867ab608b58c50
Pranavan-S <subendiranp@gmail.com>
2025-05-15 23:10:06 +0530

    Implement getUserById function

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const getUserById = require('./../controllers/userController');
+const {getUserById} = require('./../controllers/userController');
 
 router.route("/user_info/:user_id").get(getUserById);
 
