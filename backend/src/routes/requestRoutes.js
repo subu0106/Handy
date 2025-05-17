fd64b438747b051e7f52c9a5bb4904652478651a
Pranavan-S <subendiranp@gmail.com>
2025-05-17 17:16:16 +0530

    Implement updateRequestStatus function

--- backend/src/routes/requestRoutes.js
+++ backend/src/routes/requestRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createRequest, getRequestById } = require('../controllers/requestController');
+const { createRequest, getRequestById, updateRequestStatus } = require('../controllers/requestController');
 const router = express.Router();
 
 router.route("/createRequest").post(createRequest);
 router.route("/:request_id").get(getRequestById);
+router.route("/updateStatus/:request_id").put(updateRequestStatus);
+
 
 module.exports = {router};
