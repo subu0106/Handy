4b596bb415c93fee7c579c46631be18ec1b77297
vithu302001 <pavalakumaran.21@cse.mrt.ac.lk>
2025-05-25 19:45:18 +0530

    Add CORS support, Firebase integration, and user authentication features
    - Implement CORS middleware to allow all origins and specific HTTP methods.
    - Integrate Firebase for user authentication and Firestore database access.
    - Update user slice to manage user state including uid and userType.
    - Add Google sign-in functionality and registration handling in RegisterConsumer component.
    - Implement logout functionality in Profile component.
    - Refactor Providers component to use centralized Firebase configuration.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,13 @@ const pairedJobsRoutes = require("./src/routes/pairedJobsRoutes");
 const userRouter = require("./src/routes/userRoutes");
 
 const app = express();
+app.use(cors(
+    {
+        origin: '*', // Allow all origins
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+        allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+    }
+));
 
 app.use(express.json());
 app.use("/api/v1/consumers", consumerRoutes.router);
