82567b5b21063c2d317ff0d1b24b4071225d6eec
vithu302001 <pavalakumaran.21@cse.mrt.ac.lk>
2025-06-22 00:41:28 +0530

    Update CORS configuration to restrict origin to the production URL

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:5173", "http://localhost:3000", process.env.FRONTEND_URL],
-    origin: "*",
+    origin: "https://handy-eta.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
