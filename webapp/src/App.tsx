79bf5d90f74e008ed7a49c2ae7ba403dc526497e
vithu302001 <pavalakumaran.21@cse.mrt.ac.lk>
2025-06-22 18:48:28 +0530

    Fetch provider offers upon successful job acceptance

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -245,6 +245,7 @@ const App = () => {
           showToast(jobData.message, "success");
           if (user?.uid) {
             dispatch(fetchServiceRequestsBasedOnService(user.uid));
+            dispatch(fetchProviderOffers(user.uid));
           }
         });
         return () => {
